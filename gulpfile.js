649c82b26872dc33fd04681bbf8347c8b28d09fb
Hayden Bleasel <haydenbleasel@gmail.com>
2015-01-24 16:44:31 +1100

    Add gulp-favicons meta to HTML

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ gulp.task('favicons', function() {
     gulp.src('www/index.html')
         .pipe(favicons({
             files: {
-                src: 'www/media/img/noise.png',
                 dest: 'media/icons/'
             }
         }))
