ca6bd8f31c3da32fb9df7602d0cce928261a9e91
Hayden Bleasel <haydenbleasel@gmail.com>
2015-02-01 15:34:56 +1100

    Fixed gulp-favicons implementation

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 var gulp = require('gulp');
 var favicons = require('gulp-favicons');
+var del = require('del');
 
-gulp.task('default', ['favicons']);
+gulp.task('copy', function () {
+    return gulp.src('www/index.html')
+        .pipe(gulp.dest('.tmp/'));
+})
 
-gulp.task('favicons', function() {
-    gulp.src('www/index.html')
+gulp.task('favicons', ['copy'], function() {
+    return gulp.src('.tmp/index.html')
         .pipe(favicons({
             files: {
                 dest: 'media/icons/'
             }
         }))
         .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['favicons'], function () {
+    del('.tmp');
 });
