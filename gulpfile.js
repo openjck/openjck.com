c0c0726592088ac7f9b156aa41de218932c62027
John Karahalis <john.karahalis@gmail.com>
2015-11-02 20:57:05 -0800

    Optimize icons with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var imagemin = require('gulp-imagemin');
 gulp.task('build', ['build:optimize-images']);
 
 gulp.task('build:optimize-images', function() {
-    return gulp.src('www/media/img/*')
+    return gulp.src(['www/media/**/*.png', 'www/media/**/*.ico'])
                .pipe(imagemin())
-               .pipe(gulp.dest('www/build/media/img'));
+               .pipe(gulp.dest('www/build/media'));
 });
