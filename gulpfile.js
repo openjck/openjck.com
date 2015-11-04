fe1594227f2b1e7f7f0281133749a92688403c6b
John Karahalis <john.karahalis@gmail.com>
2015-11-03 21:33:03 -0800

    Progress revisioning assets with gulp-rev-all

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,38 @@
 var gulp = require('gulp');
+
 var imagemin = require('gulp-imagemin');
 var minifyCSS = require('gulp-minify-css');
+var RevAll = require('gulp-rev-all');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('build', ['build:optimize-images', 'build:minify-css']);
+var buildDirectory = 'www/build/';
 
-gulp.task('build:optimize-images', function() {
-    return gulp.src(['www/media/**/*.png', 'www/media/**/*.ico'])
-               .pipe(imagemin())
-               .pipe(gulp.dest('www/build/media'));
-});
+gulp.task('build', ['build:compress-assets', 'build:revision-assets']);
 
-gulp.task('build:minify-css', function() {
-    return gulp.src('www/media/css/main.css')
+gulp.task('build:compress-assets', ['build:compress-css', 'build:compress-images']);
+
+gulp.task('build:compress-css', function() {
+    return gulp.src('www/media/**/*.css')
                .pipe(sourcemaps.init())
                .pipe(minifyCSS())
                .pipe(sourcemaps.write('./'))
-               .pipe(gulp.dest('www/build/media/css'));
+               .pipe(gulp.dest(buildDirectory + 'media'));
+});
+
+gulp.task('build:compress-images', function() {
+    return gulp.src(['www/media/**/*.png', 'www/media/**/*.ico'])
+               .pipe(imagemin())
+               .pipe(gulp.dest(buildDirectory + 'media'));
+});
+
+gulp.task('build:copy-index', function() {
+    return gulp.src('www/index.html')
+               .pipe(gulp.dest(buildDirectory));
+});
+
+gulp.task('build:revision-assets', ['build:compress-assets', 'build:copy-index'], function() {
+    var revAll = new RevAll({ dontRenameFile: ['.html'] });
+    return gulp.src(buildDirectory + '**')
+               .pipe(revAll.revision())
+               .pipe(gulp.dest(buildDirectory))
 });
